5ad655e82ab0d34dbc0f81586a2271005e796176
jackhydez <Pash.ok92@yandex.ru>
2019-10-29 16:25:09 +0300

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+var clock = document.getElementById('clock');
+
+function myClock() {
+  var time = new Date();
+  var h = (time.getHours() % 12).toString();
+  var m = time.getMinutes().toString();
+  var s = time.getSeconds().toString();
+
+  if (h.length < 2) {
+    h = '0' + h;
+  }
+  if (m.length < 2) {
+    m = '0' + m;
+  }
+  if (s.length < 2) {
+    s = '0' + s;
+  }
+
+  var clockStr = h + ":" + m + ":" + s;
+  clock.textContent = clockStr;
+
+}
+
+myClock();
+setInterval(myClock, 1000);
+*/
